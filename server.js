f0128a4d170162f928cacd3e5725c7fd2049b623
Soumyanjalee <soumyanjaleemahapatra@gmail.com>
2016-03-05 11:24:13 +0100

    Handled POST requests in server

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app);
+var express = require('express');
+var app = express();
+var multer  = require('multer');
+var serverPort = 8080;
 
-server.listen(8080);
+//var upload = multer({ dest: 'repo/' });
 
-app.use(express.static(__dirname + '/'));
+var upload = multer({ storage : storage }).single('file');
+
+var storage =
+    multer.diskStorage({
+        destination: function (req, file, callback) {
+            callback(null, 'repo/');
+        },
+        filename: function (req, file, callback) {
+            callback(null, file.fieldname);
+        }
+    });
 
 app.get('/', function(req, res) {
-    console.log("Request received");
-    res.sendfile(__dirname + '/index.htm');
+    app.use("/", express.static(__dirname));
+    res.sendFile(__dirname + '/index.html');
 });
+
+//app.post('/upload',upload.single('file'),  function (req, res, next) {
+app.post('/upload',function (req, res, next) {
+    console.log('File uploader reached');
+    upload(req,res,function(err) {
+        //console.log(req);
+        console.log(req.files);
+        if(err) {
+            return res.end("Error uploading file.");
+            console.log('Error uploading file.');
+        }
+        res.end("File is uploaded");
+        console.log('File is uploaded');
+    });
+});
+
+app.listen(serverPort,function(){
+    console.log("Server listening on " + serverPort);
+});
+
+
